d2e96c7fd23f9268b965a53bb37d99f7eda1730b
linkwithlionhart <linkwithlionhart@gmail.com>
2023-08-12 15:35:06 -0700

    Setup Mocha-Chai test

---
+++ test/breedFetcherTest.js
@@ -0,0 +1,28 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+// Apply tests assertions.
+describe('fetchBreedDescription', () => {
+  
+  it('returns a string description for a valid breed, via callback', done => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // Expect no error for this case.
+      assert.equal(err, null);
+      const expectedDesc = 'The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.';
+      // Compare returned description.
+      assert.equal(expectedDesc, desc.trim());
+      done();
+    });
+  });
+
+  it('returns an error for an invalid breed', done => {
+    fetchBreedDescription('InvalidBreed', (err, desc) => {
+      // Expect error for this case.
+      assert.equal(err, 'Breed "InvalidBreed" not found');
+      // Expect description to be null.
+      assert.equal(desc, null);
+      done();
+    })
+  });
+
+});
