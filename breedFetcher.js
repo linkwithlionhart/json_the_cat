510b4c755f5944b7ad354845f520e45d046c0479
linkwithlionhart <linkwithlionhart@gmail.com>
2023-08-12 15:33:13 -0700

    Refactor code and transport CL block to index file

--- breedFetcher.js
+++ breedFetcher.js
@@ -34,16 +34,5 @@ const fetchBreedDescription = (breedName, callback) => {
   });
 };
 
-// Get breed name from the command line
-const breedName = process.argv[2];
-
-// Fetch the description for the provided breed name.
-fetchBreedDescription(breedName, (error, description) => {
-  if (error) {
-    // Log errors.
-    console.log('Error: ', error);
-  } else {
-    // If success, print out breed description.
-    console.log('Description: ', description);
-  }
-});
+// Export the fetchBreedDescription for external use.
+module.exports = { fetchBreedDescription }; 
